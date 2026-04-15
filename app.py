dd3883cdd65c68fba55914119825d3f4df0136fc
nhema819-arch <nhema819@gmail.com>
2026-04-15 14:51:20 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import staremlit at st
+from transformes import pipeline
+est.cache_resourse
+def load_summerize():
+  return pipeline("summairazation",model="sshleifer/distilbart-cnn-12-6")
+st.title("AI Text Summarizer")
+st.write("Enter a long text below, and get a concise Summary!")
+long_text = st.text_area("Enter text to Summarizer:", height = 200)
+max_length = st.slider("Max Summary Length",min_value=50,
+                       max_value=300, value=130)
+min_length = st.slider("Min Summary Lenght",min_value=20,
+                       max_value=100, value=30)
+with st.button("Summarize"):
+  if long_text.strip():
+    with st.spinner("Generating summary..."):
+      summary = summarize(long_text, max_lenght=max_length,
+                          min_length=min_length,do_sample=False)
+      st.subheader("Summary:")
+      st.success(summary[0]['summary_text'])
+  else:
+    st.warning("⚠️ Please enter some text to summarize.")
+    
+      
+
+
+
